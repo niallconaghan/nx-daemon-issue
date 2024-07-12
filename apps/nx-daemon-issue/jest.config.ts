2c289685ece8f377b5f61d97cbe50197e07f4bcf
Niall <niall.conaghan@gmail.com>
2024-07-12 11:56:07 +0100

    initial commit

---
+++ apps/nx-daemon-issue/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-daemon-issue',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nx-daemon-issue',
+};
